2677bf8eac88c54006198ef7d0f31423387c3a7f
Faizan Zaheer <faizanz94@hotmail.com>
2020-12-15 00:05:00 +0000

    Add files via upload

---
+++ SupermarketCheckout/Pricings.cs
@@ -0,0 +1,25 @@
+﻿using SupermarketInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketCheckout
+{
+    public class Pricings : IPricings
+    {
+        private List<Product> product = new List<Product>();
+        private List<Deal> deal = new List<Deal>();
+
+        public void AddDeal(string sku, int count, float price)
+        {
+            this.deal.Add(new Deal(sku, count, price));
+        }
+
+        public void AddProduct(string sku, float price)
+        {
+            this.product.Add(new Product(sku, price));
+        }
+    }
+}
---
+++ SupermarketCheckout/Products.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketCheckout
+{
+    class Products
+    {
+        private List<Product> products = new List<Product>();
+
+        public Products UpdateProduct(string sku, int price)
+        {
+            this.products.RemoveAll(x => x.Sku == sku);
+            this.products.Add(new Product(sku, price));
+            return this;
+        }
+
+        public Products RemoveProduct(string sku)
+        {
+            this.products.RemoveAll(x => x.Sku == sku);
+            return this;
+        }
+
+        public int GetPrice(string sku)
+        {
+            var product = this.products.Where(x => x.Sku == sku).FirstOrDefault();
+            return product.Price;
+        }
+    }
+}
