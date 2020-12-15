ab1231d3b22a16017f8c5a56e8088c9c34a93a7d
Faizan Zaheer <faizanz94@hotmail.com>
2020-12-15 00:07:37 +0000

    Add files via upload

---
+++ SupermarketCheckoutTests/PricingsTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupermarketCheckout;
+using SupermarketInterfaces;
+using System;
+namespace SupermarketCheckoutTests
+{
+    [TestClass]
+    public class PricingsTests
+    {
+        [TestMethod]
+        public void AddProduct_Successful()
+        {
+            IPricings pricings = new Pricings();
+
+            pricings.AddProduct("HGUG484JV", 1.00f);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod]
+        public void AddDeal_Successful()
+        {
+            IPricings pricings = new Pricings();
+
+            pricings.AddDeal("HGUG484JV", 3, 2.00f);
+            pricings.AddDeal("HGUG484JV", 7, 4.20f);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
