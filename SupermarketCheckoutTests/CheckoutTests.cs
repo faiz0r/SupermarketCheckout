149270196733a89903bdb258f805b7f1521f2ea9
Faizan Zaheer <faizanz94@hotmail.com>
2020-12-15 00:06:56 +0000

    Add files via upload

---
+++ SupermarketCheckoutTests/CheckoutTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupermarketCheckout;
+using SupermarketInterfaces;
+using System;
+
+namespace SupermarketCheckoutTests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        ICheckout checkout = new Checkout();
+        IPricings pricings = new Pricings();
+
+        [TestMethod]
+        public void Remove_IsTrue()
+        {
+            checkout.Remove("HGUG484JV");
+
+            Assert.IsTrue(true);
+        }
+
+        public void Empty_IsTrue()
+        {
+            checkout.Empty();
+
+        }
+
+        [TestMethod]
+        public void Scan_AreEqual()
+        {
+            checkout.Scan("HGUG484JV");
+
+        }
+    }
+}
